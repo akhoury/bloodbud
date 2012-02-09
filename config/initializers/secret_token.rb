90edbca5fe607920e0bc61684744242d67dbb38c
akhoury <bentael@gmail.com>
2012-02-09 11:11:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloodbud::Application.config.secret_token = 'ff726166cf161f496ff06ae16daa7933b9125d2111bd526a6e2fa3a90cf70c29609098725d0fc05c07cc92bad5c5ff19a3128a31d158b0ee51ebe6e0e48b1f0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bloodbud::Application.config.session_store :cookie_store, key: '_bloodbud_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bloodbud::Application.config.session_store :active_record_store
